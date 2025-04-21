40e9566823e4efe8ccdb89f1c50a923bcb25385e
YuxuanZhang271 <942326836zyx@gmail.com>
2025-04-21 22:34:48 +0800

    align mesh to pcd

--- zyx_align_smpl_2_pcd.py
+++ zyx_align_smpl_2_pcd.py
@@ -35,9 +35,14 @@ def project_joints_2_img(model_path, json_path):
         gender      = 'male'
     )
     smpl_output = smpl(**{k: v for k, v in pred_smpl_params.items()}, pose2rot=False)
-    joints = smpl_output.joints[0, :24]
+    joints = smpl_output.joints[0, :24].detach().cpu().numpy()
     # joints[:, :2] += np.array([tx, ty])
     joints += pred_cam_t_full
+
+    verts = smpl_output.vertices[0].detach().cpu().numpy()
+    verts += pred_cam_t_full
+    faces = smpl.faces.astype(np.int32)
+
     intrinsic = np.array([
         [scaled_focal_length,   0,                      W/2], 
         [0,                     scaled_focal_length,    H/2], 
@@ -48,7 +53,7 @@ def project_joints_2_img(model_path, json_path):
     joints_cam = extrinsic @ joints_h.T
     joints_img = intrinsic @ joints_cam[:3, :]
     joints_img /= joints_img[2, :]
-    return joints_img[:2, :].T
+    return joints_img[:2, :].T, joints, verts, faces
 
 
 def project_joints_2_pcd(depth_path, joints2d): 
@@ -65,13 +70,32 @@ def project_joints_2_pcd(depth_path, joints2d):
     return np.stack(joints, axis=0)
 
 
+def rigid_transform_3D(A: np.ndarray, B: np.ndarray):
+    assert A.shape == B.shape
+    centroid_A = A.mean(axis=0)
+    centroid_B = B.mean(axis=0)
+    AA = A - centroid_A
+    BB = B - centroid_B
+
+    H = AA.T @ BB
+    U, S, Vt = np.linalg.svd(H)
+    R = Vt.T @ U.T
+    # fix reflection
+    if np.linalg.det(R) < 0:
+        Vt[2, :] *= -1
+        R = Vt.T @ U.T
+    t = centroid_B - R @ centroid_A
+    
+    return R, t
+
+
 def main():
     record_time = 20250418005607
-    timestamp = 10642911
+    timestamp = 10639122
 
     model_path = 'models/smpl'
     json_path = f'demo_out/{record_time}/{timestamp}_0.json'
-    joints2d = project_joints_2_img(model_path, json_path)
+    joints2d, joints, verts, faces = project_joints_2_img(model_path, json_path)
 
     # image = cv2.imread(f'/home/yuxuanzhang/Documents/Projects/pyorbbecsdk/records/{record_time}/color_images/{timestamp}.png')
 
@@ -91,8 +115,36 @@ def main():
 
     depth_path = f'/home/yuxuanzhang/Documents/Projects/pyorbbecsdk/records/{record_time}/depth_images/{timestamp}.raw'
     joints3d = project_joints_2_pcd(depth_path, joints2d)
-    print(joints3d.shape)
+    # print(joints3d.shape)
 
+    R, t = rigid_transform_3D(joints, joints3d)
+    residuals = np.linalg.norm((joints @ R.T + t) - joints3d, axis=1)
+    thresh = 0.5
+    inliers = residuals < thresh
+    if inliers.sum() >= 3:
+        R_refined, t_refined = rigid_transform_3D(
+            joints[inliers],
+            joints3d[inliers]
+        )
+        R, t = R_refined, t_refined
+    else:
+        print("Too few inliers for refinement, using initial fit")
+
+    backoff = -0.1
+    v_forward = R[:, 2]  
+    t_adjusted = t - v_forward * backoff
+    T = np.eye(4)
+    T[:3, :3] = R
+    T[:3,  3] = t_adjusted
+    
+    mesh = o3d.geometry.TriangleMesh(
+        o3d.utility.Vector3dVector(verts),
+        o3d.utility.Vector3iVector(faces),
+    )
+    mesh.compute_vertex_normals()
+    mesh.transform(T)
+
+    # visualization
     joint_spheres = []
     for xyz in joints3d:
         sphere = o3d.geometry.TriangleMesh.create_sphere(radius=0.02)
@@ -105,7 +157,7 @@ def main():
     pts /= 1000.0
     point_cloud.points = o3d.utility.Vector3dVector(pts)
 
-    o3d.visualization.draw_geometries([point_cloud, *joint_spheres])
+    o3d.visualization.draw_geometries([point_cloud, *joint_spheres, mesh])
 
 if __name__ == '__main__': 
     main()
